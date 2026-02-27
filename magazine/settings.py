ec3256df1be93ffed525dfa75699103c958b59ae
Thivyesh Sivasuriyamoorthy <thivyesh.sivasuriyamoorthy@student.manchester.ac.uk>
2026-02-27 20:55:45 +0000

    refix

--- magazine/settings.py
+++ magazine/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ---- SECURITY ----
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-before-deploying')
-DEBUG = os.environ.get('DEBUG', 'True') == 'False'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 # ---- APPS ----
